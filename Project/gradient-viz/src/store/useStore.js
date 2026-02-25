7d6fe1c714462b70fdb190b336e798ebc343cd4d
rankirat-singh <88544263+rankirat-singh@users.noreply.github.com>
2026-02-25 23:38:09 +0530

    Implemented Hooks and Started with the visualization

---
+++ Project/gradient-viz/src/store/useStore.js
@@ -0,0 +1,22 @@
+import {create} from 'zustand';
+const useStore = create((set) => ({
+    // State
+    viewMode: '3d-explore',
+    personPosition: [1.0, 1.0],
+    showAscentPath: false,
+    ascentProgress: 0,
+
+    //Actions
+    setviewMode: (mode) => set({viewMode: mode}),
+    setPersonPosition: (pos) => set({personPosition: pos}),
+    setShowAscentPath: (show) => set({showAscentPath: show}),
+    setAscentProgress: (progress) => set({ascentProgress : progress}),
+    reset: () => set({
+        viewMode: '3d-explore',
+        personPosition: [1.0, 1.0],
+        showAscentPath: false,
+        ascentProgress: 0,
+    }),
+}));
+
+export default useStore;
