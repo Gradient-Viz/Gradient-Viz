34595e15c494fcdb66f5139983d7650d030d84eb
Sid Kona <konas@student.douglascollege.ca>
2026-03-18 15:23:24 -0700

    fixed first person view and buttons to switch between views

--- Project/gradient-viz/src/components/UIOverlay.jsx
+++ Project/gradient-viz/src/components/UIOverlay.jsx
@@ -50,6 +50,9 @@ export default function UIOverlay(){
     const handleReturnTo3D = () => setViewMode('3d_compare');
     const handleReset = () => reset();
 
+    const handleSwitchToFirstPerson = () => setViewMode('first_person');
+    const handleExitFirstPerson = () => setViewMode('3d_compare');
+
     return (
         <div className='sidebar'>
             {/* Header */}
@@ -204,6 +207,24 @@ export default function UIOverlay(){
                     Reset
                 </button>
             )}
+
+        {/* View mode buttons */}
+        {viewMode === '3d_explore' && (
+            <>
+                {/* Enter First Person button */}
+                        <button className="btn-primary" onClick={handleSwitchToFirstPerson}>
+                    First Person View
+                </button>
+            </>
+        )}
+
+        {/* Exit First Person button */}
+        {viewMode === 'first_person' && (
+            <button className="btn-primary" onClick={handleExitFirstPerson}>
+                Exit First Person
+            </button>
+        )}
+
         </div>
 
         {/* Info */}
