eacd3a667cc2128e4b3ca479e8917ec0aeac1b23
Sid <konas@student.douglascollege.ca>
2026-04-14 18:50:51 -0700

    Update CameraController.jsx
    Fixing camera snap back

--- Project/gradient-viz/src/components/CameraController.jsx
+++ Project/gradient-viz/src/components/CameraController.jsx
@@ -94,7 +94,7 @@ export default function CameraController(){
             window.removeEventListener('keyup', handleKeyUp);
         };
       
-    }, [viewMode, camera, personPosition]);
+    }, [viewMode]);
 
 
     useEffect(() => {
